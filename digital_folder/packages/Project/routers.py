a971e7896bce6f7735392bc9ce6aa63193fd09fe
lfms6164 <morim.leonardo2@gmail.com>
2025-03-03 12:31:23 +0000

    Create new endpoint for image upload

--- digital_folder/packages/Project/routers.py
+++ digital_folder/packages/Project/routers.py
@@ -1,7 +1,9 @@
-from typing import List
+import os
+from typing import Any, List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, File, HTTPException, UploadFile
 
+from digital_folder.core.config import project_settings
 from digital_folder.packages.Project.dto import ProjectDTO
 from digital_folder.packages.Project.schemas import (
     ProjectOut,
@@ -50,3 +52,31 @@ class ProjectRouter:
         """Delete project"""
 
         return ProjectDTO.delete_by_id(project_id)
+
+    @staticmethod
+    @project_router.post(path="/upload_image")
+    async def upload_image(file: UploadFile = File(...)) -> Any:
+        """Handles image upload and returns the saved filename"""
+
+        upload_dir = project_settings.static_folder_path
+        if not os.path.exists(upload_dir):
+            raise HTTPException(
+                status_code=400, detail=f"Directory does not exist: {upload_dir}"
+            )
+
+        file_extension = file.filename.split(".")[-1].lower()
+        if file_extension not in ["png", "jpg", "jpeg", "gif"]:
+            raise HTTPException(
+                status_code=400, detail="Upload image failed - Invalid file type"
+            )
+
+        existing_files = set(os.listdir(upload_dir))
+
+        if file.filename not in existing_files:
+            file_path = os.path.join(upload_dir, file.filename)
+
+            # Save file
+            with open(file_path, "wb") as buffer:
+                buffer.write(await file.read())
+
+        return {"filename": file.filename}
